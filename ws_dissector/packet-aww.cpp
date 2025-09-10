55407152595fca5cc04959af95c79b8c53624d6b
Ajax Dong <i@aajax.top>
2025-09-10 10:20:40 +0800

    Port ws_dissector to WireShark 4.4.9

--- ws_dissector/packet-aww.cpp
+++ ws_dissector/packet-aww.cpp
@@ -120,33 +120,33 @@ dissect_extra_pdcp_lte_info(tvbuff_t *tvb, packet_info *pinfo, gint32 offset)
 
 
     /* Read fixed fields */
-    p_pdcp_lte_info->no_header_pdu = (gboolean)tvb_get_guint8(tvb, offset++);
-    p_pdcp_lte_info->plane = (enum pdcp_plane)tvb_get_guint8(tvb, offset++);
+    p_pdcp_lte_info->no_header_pdu = (gboolean)tvb_get_uint8(tvb, offset++);
+    p_pdcp_lte_info->plane = (enum pdcp_plane)tvb_get_uint8(tvb, offset++);
     if (p_pdcp_lte_info->plane == SIGNALING_PLANE) {
         p_pdcp_lte_info->seqnum_length = PDCP_SN_LENGTH_5_BITS;
     }
-    p_pdcp_lte_info->rohc.rohc_compression = (gboolean)tvb_get_guint8(tvb, offset++);
+    p_pdcp_lte_info->rohc.rohc_compression = (gboolean)tvb_get_uint8(tvb, offset++);
 
     /* Read optional fields */
     while (tag != PDCP_LTE_PAYLOAD_TAG) {
         /* Process next tag */
-        tag = tvb_get_guint8(tvb, offset++);
+        tag = tvb_get_uint8(tvb, offset++);
         switch (tag) {
             case PDCP_LTE_SEQNUM_LENGTH_TAG:
-                p_pdcp_lte_info->seqnum_length = tvb_get_guint8(tvb, offset);
+                p_pdcp_lte_info->seqnum_length = tvb_get_uint8(tvb, offset);
                 offset++;
                 seqnumLengthTagPresent = TRUE;
                 break;
             case PDCP_LTE_DIRECTION_TAG:
-                p_pdcp_lte_info->direction = tvb_get_guint8(tvb, offset);
+                p_pdcp_lte_info->direction = tvb_get_uint8(tvb, offset);
                 offset++;
                 break;
             case PDCP_LTE_LOG_CHAN_TYPE_TAG:
-                p_pdcp_lte_info->channelType = (LogicalChannelType)tvb_get_guint8(tvb, offset);
+                p_pdcp_lte_info->channelType = (LogicalChannelType)tvb_get_uint8(tvb, offset);
                 offset++;
                 break;
             case PDCP_LTE_BCCH_TRANSPORT_TYPE_TAG:
-                p_pdcp_lte_info->BCCHTransport = (BCCHTransportType)tvb_get_guint8(tvb, offset);
+                p_pdcp_lte_info->BCCHTransport = (BCCHTransportType)tvb_get_uint8(tvb, offset);
                 offset++;
                 break;
             case PDCP_LTE_ROHC_IP_VERSION_TAG:
@@ -154,23 +154,23 @@ dissect_extra_pdcp_lte_info(tvbuff_t *tvb, packet_info *pinfo, gint32 offset)
                 offset += 2;
                 break;
             case PDCP_LTE_ROHC_CID_INC_INFO_TAG:
-                p_pdcp_lte_info->rohc.cid_inclusion_info = tvb_get_guint8(tvb, offset);
+                p_pdcp_lte_info->rohc.cid_inclusion_info = tvb_get_uint8(tvb, offset);
                 offset++;
                 break;
             case PDCP_LTE_ROHC_LARGE_CID_PRES_TAG:
-                p_pdcp_lte_info->rohc.large_cid_present = tvb_get_guint8(tvb, offset);
+                p_pdcp_lte_info->rohc.large_cid_present = tvb_get_uint8(tvb, offset);
                 offset++;
                 break;
             case PDCP_LTE_ROHC_MODE_TAG:
-                p_pdcp_lte_info->rohc.mode = (enum rohc_mode)tvb_get_guint8(tvb, offset);
+                p_pdcp_lte_info->rohc.mode = (enum rohc_mode)tvb_get_uint8(tvb, offset);
                 offset++;
                 break;
             case PDCP_LTE_ROHC_RND_TAG:
-                p_pdcp_lte_info->rohc.rnd = tvb_get_guint8(tvb, offset);
+                p_pdcp_lte_info->rohc.rnd = tvb_get_uint8(tvb, offset);
                 offset++;
                 break;
             case PDCP_LTE_ROHC_UDP_CHECKSUM_PRES_TAG:
-                p_pdcp_lte_info->rohc.udp_checksum_present = tvb_get_guint8(tvb, offset);
+                p_pdcp_lte_info->rohc.udp_checksum_present = tvb_get_uint8(tvb, offset);
                 offset++;
                 break;
             case PDCP_LTE_ROHC_PROFILE_TAG:
